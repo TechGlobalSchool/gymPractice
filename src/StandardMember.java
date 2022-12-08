23940ef8ed234f792edeb43522c39fe29e0250c4
TechGlobalSchool <tgschool01@gmail.com>
2022-12-07 19:29:43 -0600

    ..

--- src/StandardMember.java
+++ src/StandardMember.java
@@ -1,8 +1,10 @@
-public class StandardMember extends Member{
+public class StandardMember extends Member {
 
     public StandardMember(String firstName, String lastName, String memberId, int age, double height, double weight, String membershipType) {
         super(firstName, lastName, memberId, age, height, weight, membershipType);
     }
+
+    public static final String membershipType = "Standard";
 
     @Override
     public void workingOut(String duration) {
@@ -19,5 +21,28 @@ public class StandardMember extends Member{
     public void buyingProducts(double money) {
         System.out.println("Amount due = " + money +
                 "\nYou don't have any discounts!");
+    }
+
+    public static StandardMember getStandardMember() {
+        String firstName = Values.firstNames.get(
+                GeneratorUtils.getRandomNumber(0, Values.firstNames.size() - 1)
+        );
+        String lastName = Values.lastNames.get(
+                GeneratorUtils.getRandomNumber(0, Values.lastNames.size() - 1)
+        );
+        String memberId = GeneratorUtils.getMemberId(membershipType);
+        int age = GeneratorUtils.getAge();
+        double height = GeneratorUtils.getHeight();
+        double weight = GeneratorUtils.getWeight();
+
+        return new StandardMember(firstName, lastName, memberId, age, height, weight, membershipType);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getStandardMember());
+        System.out.println();
+        System.out.println(getStandardMember());
+        System.out.println();
+        System.out.println(getStandardMember());
     }
 }
