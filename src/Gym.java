d04d36de5d4e1c7d03b96e218a9f86b2eee150f3
TechGlobalSchool <tgschool01@gmail.com>
2022-12-07 20:12:13 -0600

    ..

--- src/Gym.java
+++ src/Gym.java
@@ -4,18 +4,32 @@ public class Gym {
 
         //3 standard members
         for (int i = 0; i < 3; i++) {
+            StandardMember member = StandardMember.getStandardMember();
             //scan the member
-
+            member.scanning();
             //let them workout
-
+            member.workingOut("30 minutes");
             //make them buy products
-
+            member.buyingProducts(20.0);
             //print the standard member
+            System.out.println(member);
+            System.out.println("--------------------------------");
         }
 
         //3 premium members
 
+        for (int i = 0; i < 3; i++) {
+            //scan the member
+            //let them workout
+            //make them buy products
+            //get massage
+            //get tanned
+            //print the standard member
+
+            System.out.println("--------------------------------");
+        }
 
         //create 5 random members (standard or premium)
+        //
     }
 }
